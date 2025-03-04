71b34dbb2bd5ae2bdc749965f371c3dee939991c
Nissan Barua <baruanissan55@gmail.com>
2025-03-04 20:02:09 +0600

    get count data  from database

--- index.js
+++ index.js
@@ -31,6 +31,11 @@ async function run() {
       const result = await productCollection.find().toArray();
       res.send(result);
     });
+    // product count set
+    app.get("/productsCount", async (req, res) => {
+      const count = await productCollection.estimatedDocumentCount();
+      res.send({ count });
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
